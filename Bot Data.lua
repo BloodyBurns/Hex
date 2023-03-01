774033b8d359b7135ccfb1d0aa9551443e58c7bc
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2023-02-28 20:46:15 -0500

    Create Bot Data.lua

---
+++ Bot Data.lua
@@ -0,0 +1,20 @@
+return {
+    Cache = {},
+    Name = {
+        'TheAloneSystems_', 'StormWRLD_', 'D00MBotMan_', 'newgenLOLOLOL', 'skidbottingera', 'sleepinbot', 'sleepnbetter', 'MurderlnOsaka', 'iluvlostritualac',
+        'LOSTRITUALOWNSYOU', 'prunebot', 'skidbottingera', 'killhouseAC7', 'llllllllIIIllIIllI', 'CaughtHisAssLackin', 'PicOfUrAssOnDoxbin', 'SniperRifleScope',
+        'AgriculturePlants', 'executiveauthority', 'AllowanceMoney', 'emergencyelevator', 'AlwaysSmokinWax', 'bottedby_kxt5970', 'KillaSwats', 'KillaExtorts',
+        'ILoveBwc', 'WHOLELOTTAHOLDER', 'egirlb2oobs', 'Hexaria_R20', 'kronebot', 'bottedby_kxt5970', 'mistamisery', 'NETWORKRUNSU', 'cutesillybak_',
+        'TheLongrangeRifle_', 'HOLDINGNEXT', 'yieviro92creepy'
+    },
+
+    Display = {
+        'TheAloneSystems', 'sleepinbot', 'sleepnbetter', 'ILuvOven', 'skidbottingera2', 'LOSTRITUALOWNSYOU', 'prunebot', 'yieviro92creepy',
+        'Killhousebot', 'MestalicFlungYouLOL', 'blowupsASSisLARGE', 'Deskoard_9s7R7GdUGz', 'kronebot', 'improved01', 'NETWORK', 'cutesillybak_', 'SnipedByCyber'
+    },
+
+    FilterMessage = {
+        '.gg/shhh', '| Hold', 'dis//shh', '@krone', '.gg/gravesite', '@murder', '.ggvwl', 'purchase whitelist', 'purchase a whitelist',
+        '@gesture', 'gesture#', '.gg/gravesite', 'Tox1chappy', 'nAdwzx7Bsk', 'join for '
+    }
+};
